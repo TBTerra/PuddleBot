8dc71d42dd4eed98122e2aa858f5b5c9f0dd1205
Unknown <terra@rudi.org.uk>
2018-08-06 01:40:10 +0100

    functional layout

--- cogs/guess.py
+++ cogs/guess.py
@@ -14,15 +14,25 @@ class Guess:
 		try:
 			with open('guess-state.json') as f:
 				self.state = json.load(f)
+			with open('guess-data/pages.json') as f:
+				self.pages = json.load(f)
 		except:
 			print('no game state, making blank one')
-			self.state = {}
+			self.state = {'curImg':0,'lastTime':0,'posX':-1, 'posY':-1, 'hintLV':0}
 
 	def newPage(ctx):
+		##choose a new page
+		##choose cropping
+		##generate crop
+		##update/save gamestate
+		##output new hint
 		return await ctx.send('Not yet implimented',delete_after=10)
 		
 	def correct(ctx):
+		##generate image with hint area highlighted
+		##output that
 		await ctx.send('Not yet implimented',delete_after=10)
+		##make a new hint
 		return newPage(ctx)
 		
 	
@@ -30,16 +40,20 @@ class Guess:
 	async def guess(self, ctx):
 		"""A group of commands for the Comic Guess Game. use {prefix}help guess for more details"""
 		if ctx.invoked_subcommand is None:
-			return await ctx.send('Not yet implimented',delete_after=10)
+			return await ctx.send('No sub-command recognised. use {prefix}help guess to see avalable sub-commands',delete_after=10)
 
 	@guess.command()
 	async def hint(self, ctx):
 		"""Gives a larger hint for the current page (only works after 5 mins)"""
+		##check if its been 5 mins
+		##make a bigger hint of the current page
 		return await ctx.send('Not yet implimented',delete_after=10)
 
 	@guess.command()
 	async def skip(self, ctx):
 		"""Skip to the next random page (only works after 1 hour)"""
+		##check if its been 1 hour
+		##make a new page
 		return await ctx.send('Not yet implimented',delete_after=10)
 
 	@guess.command(aliases=["cur"])
