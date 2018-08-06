ca5c89611c84242f881afec9fb984c3c9ba75dda
Unknown <terra@rudi.org.uk>
2018-08-06 17:39:46 +0100

    guess implimentation

--- cogs/guess.py
+++ cogs/guess.py
@@ -2,39 +2,58 @@ import discord
 from discord.ext.commands import group
 from discord.ext.commands import bot
 from discord.ext import commands
-import requests
-import cfg
 import json
-
+import random
+import time
+from PIL import Image, ImageDraw
 
 class Guess:
 	def __init__(self, bot):
 		self.bot = bot
 		#load gamestate
+		with open('guess-data/pages.json') as f:
+			self.pages = json.load(f)
 		try:
 			with open('guess-state.json') as f:
 				self.state = json.load(f)
-			with open('guess-data/pages.json') as f:
-				self.pages = json.load(f)
 		except:
 			print('no game state, making blank one')
 			self.state = {'curImg':0,'lastTime':0,'posX':-1, 'posY':-1, 'hintLV':0}
 
-	def newPage(ctx):
+	async def newPage(self,ctx):
 		##choose a new page
+		imgNum = random.randint(0, len(self.pages)-1)
+		imgName = 'guess-data/{:04d}.png'.format(int(self.pages[imgNum][0][3:]))
+		try:
+			original = Image.open(imgName)
+		except:
+			print('Unable to load image {}'.format(imgName))
+			return await ctx.send('Something went wrong, ask Terra to look at the logs',delete_after=10)
 		##choose cropping
+		X = random.randint(75,original.size[0]-75)
+		Y = random.randint(75,original.size[1]-75)
 		##generate crop
+		original.crop((X-75,Y-75,X+76,Y+76)).save("hint.png")
 		##update/save gamestate
+		self.state = {'curImg':imgNum,'lastTime':time.time(),'posX':X, 'posY':Y, 'hintLV':0}
+		with open('guess-state.json','w') as f:
+			json.dump(self.state,f)
 		##output new hint
-		return await ctx.send('Not yet implimented',delete_after=10)
+		return await ctx.send('New Hint:', file=discord.File('hint.png'))
 		
-	def correct(ctx):
+	async def Correct(self,ctx):
 		##generate image with hint area highlighted
+		imgName = 'guess-data/{:04d}.png'.format(int(self.pages[self.state['curImg']][0][3:]))
+		original = Image.open(imgName)
+		draw = ImageDraw.Draw(original)
+		X = self.state['posX']
+		Y = self.state['posY']
+		draw.rectangle((X-(75+self.state['hintLV']),Y-(75+self.state['hintLV']),X+(76+self.state['hintLV']),Y+(76+self.state['hintLV'])),outline = '#CC0000')
+		original.save('overlay.png')
 		##output that
-		await ctx.send('Not yet implimented',delete_after=10)
+		await ctx.send('That is correct',file=discord.File('overlay.png'))
 		##make a new hint
-		return newPage(ctx)
-		
+		return await Guess.newPage(self,ctx)
 	
 	@group(pass_context=True)
 	async def guess(self, ctx):
@@ -46,35 +65,75 @@ class Guess:
 	async def hint(self, ctx):
 		"""Gives a larger hint for the current page (only works after 5 mins)"""
 		##check if its been 5 mins
-		##make a bigger hint of the current page
-		return await ctx.send('Not yet implimented',delete_after=10)
-
+		if (time.time()-self.state['lastTime']) >= 300:
+			self.state['hintLV'] += 25
+			self.state['lastTime'] = time.time()
+			with open('guess-state.json','w') as f:
+				json.dump(self.state,f)
+			##make a bigger hint of the current page
+			imgName = 'guess-data/{:04d}.png'.format(int(self.pages[self.state['curImg']][0][3:]))
+			original = Image.open(imgName)
+			##generate crop
+			X = self.state['posX']
+			Y = self.state['posY']
+			original.crop((X-(75+self.state['hintLV']),Y-(75+self.state['hintLV']),X+(76+self.state['hintLV']),Y+(76+self.state['hintLV']))).save("hint.png")
+			##output new hint
+			return await ctx.send('New Hint:', file=discord.File('hint.png'))
+		else:
+			return await ctx.send('It hasnt been 5 minutes, can\'t get a hint yet\ntime remaining ({})'.format(300-(time.time()-self.state['lastTime'])))
 	@guess.command()
 	async def skip(self, ctx):
 		"""Skip to the next random page (only works after 1 hour)"""
 		##check if its been 1 hour
-		##make a new page
-		return await ctx.send('Not yet implimented',delete_after=10)
+		if (time.time()-self.state['lastTime']) >= 3600:
+			##make a new page
+			return await Guess.newPage(self,ctx)
+		else:
+			return await ctx.send('It hasnt been an hour, can\'t skip yet',delete_after=10)
 
 	@guess.command(aliases=["cur"])
 	async def current(self, ctx):
 		"""Gives the current hint for the current page"""
-		return await ctx.send('Not yet implimented',delete_after=10)
+		return await ctx.send('Current Hint:', file=discord.File('hint.png'))
 
 	@guess.command()
 	async def da(self, ctx, *, page):
 		"""Make a guess at the current page using a DA page number"""
-		return await ctx.send('Not yet implimented',delete_after=10)
+		try:
+			num = int(page)
+		except:
+			return await ctx.send('Expecting a page number for the guess',delete_after=10)
+		gess = 'da {}'.format(num)
+		if gess in self.pages[self.state['curImg']]:
+			return await Guess.Correct(self,ctx)
+		else:
+			return await ctx.send('Thats not correct',delete_after=10)
 
 	@guess.command()
 	async def cf(self, ctx, *, page):
 		"""Make a guess at the current page using a CF page number"""
-		return await ctx.send('Not yet implimented',delete_after=10)
+		try:
+			num = int(page)
+		except:
+			return await ctx.send('Expecting a page number for the guess',delete_after=10)
+		gess = 'cf {}'.format(num)
+		if gess in self.pages[self.state['curImg']]:
+			return await Guess.Correct(self,ctx)
+		else:
+			return await ctx.send('Thats not correct',delete_after=10)
 
 	@guess.command()
 	async def sj(self, ctx, *, page):
 		"""Make a guess at the current page using a SJ page number"""
-		return await ctx.send('Not yet implimented',delete_after=10)
+		try:
+			num = int(page)
+		except:
+			return await ctx.send('Expecting a page number for the guess',delete_after=10)
+		gess = 'sj {}'.format(num)
+		if gess in self.pages[self.state['curImg']]:
+			return await Guess.Correct(self,ctx)
+		else:
+			return await ctx.send('Thats not correct',delete_after=10)
 
 def setup(bot):
 	bot.add_cog(Guess(bot))
