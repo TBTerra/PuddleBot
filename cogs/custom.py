ec87a6a36da28ed302957f94c75d9b88012cba35
Unknown <terra@rudi.org.uk>
2019-02-07 00:22:00 +0000

    Added dev cog for dev commands

--- cogs/custom.py
+++ cogs/custom.py
@@ -212,24 +212,6 @@ class Custom:
 			list += '{}\n'.format(key)
 		list = list[:-1] + '```'
 		return await ctx.send(list)
-	
-	@commands.command()
-	@commands.is_owner()
-	async def setpower(self, ctx, *, role: discord.Role = None):
-		""""set privilege level (bot owner only)"""
-		if role is None:
-			return await ctx.send('I\'m sorry {}. I\'m afraid I can\'t do that :confused:\nYou haven\'t specified a role!'.format(ctx.author.nick if ctx.author.nick!=None else ctx.author.name),delete_after=5)
-		if role not in ctx.message.guild.roles:
-			return await ctx.send('I\'m sorry {}. I\'m afraid I can\'t do that :confused:\nThat role dosnt exist!'.format(ctx.author.nick if ctx.author.nick!=None else ctx.author.name),delete_after=5)
-		cfg.bot['power-role'] = role.id
-		cfg.savecfg()
-		
-	
-	@commands.command()
-	@commands.is_owner()
-	async def say(self, ctx, *, convert):
-		await ctx.send(convert)
-		return await ctx.message.delete()
 
 def setup(bot):
 	bot.add_cog(Custom(bot))
---
+++ cogs/dev.py
@@ -0,0 +1,37 @@
+import discord
+import cfg
+from discord.ext import commands
+
+class Dev:
+	def __init__(self, bot):
+		self.bot = bot
+
+	@commands.command()
+	@commands.is_owner()
+	async def setpower(self, ctx, *, role: discord.Role = None):
+		""""set privilege level (bot owner only)"""
+		if role is None:
+			return await ctx.send('I\'m sorry {}. I\'m afraid I can\'t do that :confused:\nYou haven\'t specified a role!'.format(ctx.author.nick if ctx.author.nick!=None else ctx.author.name),delete_after=5)
+		if role not in ctx.message.guild.roles:
+			return await ctx.send('I\'m sorry {}. I\'m afraid I can\'t do that :confused:\nThat role dosnt exist!'.format(ctx.author.nick if ctx.author.nick!=None else ctx.author.name),delete_after=5)
+		cfg.bot['power-role'] = role.id
+		cfg.savecfg()
+	
+	@commands.command()
+	@commands.is_owner()
+	async def role(self, ctx, *, role: discord.Role = None):
+		"""grab the ID of the role specified (bot owner only)"""
+		if role is None:
+			return await ctx.send('I\'m sorry {}. I\'m afraid I can\'t do that :confused:\nYou haven\'t specified a role!'.format(ctx.author.nick if ctx.author.nick!=None else ctx.author.name),delete_after=5)
+		if role not in ctx.message.guild.roles:
+			return await ctx.send('I\'m sorry {}. I\'m afraid I can\'t do that :confused:\nThat role dosnt exist!'.format(ctx.author.nick if ctx.author.nick!=None else ctx.author.name),delete_after=5)
+		return await ctx.send('That Role has the ID: {} and has {} members'.format(role.id, len(role.members)))
+
+	@commands.command()
+	@commands.is_owner()
+	async def say(self, ctx, *, convert):
+		await ctx.send(convert)
+		return await ctx.message.delete()
+
+def setup(bot):
+	bot.add_cog(Dev(bot))
